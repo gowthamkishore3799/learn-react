c11ef013f317d22f409ed87c8d016e5e88e252a5
gowthamkishore <gowthamkishore3@gmail.com>
2025-07-03 23:31:23 -0700

    Adding changes

--- app/routes/newDashboard.tsx
+++ app/routes/newDashboard.tsx
@@ -59,7 +59,7 @@ function Body(){
         label: renderOptions("RED"), value: "Red"
     }]
     return (
-      <div>
+      <div className="body-title">
         <div className="body-heading">
           <span> TODO </span>
         </div>
@@ -172,12 +172,44 @@ Red
       </div>
     );
 }
+
+function Footer(){
+  return (<>
+  <div className="flex flex-row justify-center items-center">
+    <div className="">
+    <svg
+    fill="#000000"
+    width="100px"
+    height="100px"
+    viewBox="0 0 24 24"
+    id="star"
+    data-name="Line Color"
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon line-color"
+  >
+    <polygon
+      id="primary"
+      points="12 4 9.22 9.27 3 10.11 7.5 14.21 6.44 20 12 17.27 17.56 20 16.5 14.21 21 10.11 14.78 9.27 12 4"
+      style={{
+        fill: "none",
+        stroke: "#FF0000",
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeWidth: 2,
+      }}
+    />
+  </svg>
+      </div>
+  </div>
+  </>)
+}
 export default function Dashboard() {
   return (
     <>
     <div className="dashboard">
     <Header/>
     <Body/>
+    <Footer/>
     </div>
     </>
   );
